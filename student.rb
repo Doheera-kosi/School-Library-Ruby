3c5c58cb840b324cfc65bdaded43251227be08ae
Doheera-kosi <kupourevans99@gmail.com>
2022-12-28 15:46:40 +0000

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
-require './person'
+require_relative './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     @classroom = classroom
     super(age, name, parent_permission)
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hookey
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.student.include?(self)
   end
 end
