3db983b835454cb7df677c62c3ee1cc61a76071f
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-12 00:39:00 +0500

    Writing person and student tests

---
+++ spec/student_spec.rb
@@ -0,0 +1,13 @@
+require '../classes/student.rb'
+
+describe Student do
+    student = Student.new(24, true, 'Evan')
+    describe "Student Information" do
+        it "should have a type of Student" do
+            expect(student.type).to eql("Student")
+        end
+        it "should have a shape of '¯\(ツ)/¯'" do
+            expect(student.play_hookey).to eql('¯\(ツ)/¯')
+        end
+    end
+end
